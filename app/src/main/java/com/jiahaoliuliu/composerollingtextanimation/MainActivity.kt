cf6fe4e8286443ec4eb13dbff80b61839ff8c200
Jiahao Liu <jiahao.liu@glovoApp.com>
2025-12-04 22:57:07 +0100

    Created the counter

--- app/src/main/java/com/jiahaoliuliu/composerollingtextanimation/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/composerollingtextanimation/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.jiahaoliuliu.composerollingtextanimation
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
 import com.jiahaoliuliu.composerollingtextanimation.ui.theme.ComposeRollingTextAnimationTheme
 
+// Sample code from https://www.youtube.com/watch?v=07ZdBCyh7sc
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +38,67 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeRollingTextAnimationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        var count by remember {
+                            mutableStateOf(0)
+                        }
+                        AnimatedCounter(
+                            count = count,
+                        )
+                        Button(onClick = { count++ }) {
+                            Text(text = "increment")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AnimatedCounter(
+    modifier: Modifier = Modifier,
+    count: Int,
+    style: TextStyle = MaterialTheme.typography.bodyLarge,
+) {
+    var oldCount by remember {
+        mutableStateOf(count)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeRollingTextAnimationTheme {
-        Greeting("Android")
+    // This will be called after every recomposition
+    SideEffect {
+        oldCount = count
+    }
+
+    Row(modifier = modifier) {
+        val countString = count.toString()
+        val oldCountString = oldCount.toString()
+        for (i in countString.indices) {
+            val oldChar = oldCountString.getOrNull(i)
+            val newChar = countString[i]
+            val char = if (oldChar == newChar) {
+                oldCountString[i]
+            } else {
+                countString[i]
+            }
+            AnimatedContent(
+                targetState = char,
+                transitionSpec = {
+                    slideInVertically { it }.togetherWith(slideOutVertically { -it })
+                }
+            ) {
+                Text(
+                    text = char.toString(),
+                    style = style,
+                    softWrap = false,
+                )
+            }
+        }
     }
 }
